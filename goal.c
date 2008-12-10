b7d6c6436c59dd32f238c7dd0285049e913d73ee
Andrew Brown <brownan@gmail.com>
2008-12-10 00:18:49 -0500

    More goal written, still incomplete.  See note towards end of goal.c

--- goal.c
+++ goal.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "stack.h"
+#include "cube.h"
 
 /*
  * as always, see the header file for more comments
@@ -18,10 +19,21 @@ int goal_solve(const char *scrambled, const char *solved,
     
     qdata current;
     cube_type turned;
-    int i;
+    int i, j;
     stacktype *stack;
     int depth = -1;
 
+    /*
+     * Vars declared used later by heuristics
+     */
+    qdata turns[18];
+    int heuristics[18];
+    int numturns;
+    int heuristic;
+    /* int heu2; */
+    int hash;
+    qdata temp;
+    int f;
 
     /*
      * primitive stack to hold the solution
@@ -90,7 +102,90 @@ int goal_solve(const char *scrambled, const char *solved,
              * not at depth yet, turn this cube all about, applying heuristics
              * pruning
              */
-            /* TODO */
+            numturns = 0;
+            for (i=0; i<18; ++i) {
+                
+                /* Determine if we should skip this turn */
+                if (current.turn != -1 && SHOULDIAVOID(i, current.turn)) {
+                    continue;
+                }
+                
+                /*
+                 * make a copy for this turn and turn it
+                 */
+                memcpy(turned, current.cube_data, CUBELEN);
+                cube_turn(turned, i);
+
+                /*
+                 * Now get the heuristic value for this cube
+                 */
+                heuristic = 0;
+                hash = corner_hash(turned);
+                heuristic = (hash&1 ? \
+                        (cornertable[(hash-1)/2] >> 4) : \
+                        (cornertable[hash/2] & 15) );
+#ifdef DEBUG_ASSERTS
+                if (heuristic < 0 || heuristic > 11) {
+                    fprintf(stderr, "\nWARNING: CORNER HERUISTIC OUT OF BOUNDS\n");
+                    hash = *((int *)0x0);
+                }
+#endif
+
+                /*
+                 * Here, we would put the next heuristic value into
+                 * heu2
+                 * and set heuristic to max(heruistic,heu2)
+                 *
+                 * repeat as necessary
+                 */
+
+                /* The turned node has distance-plus-cost value
+                 * f(x) = g(x) + h(x)
+                 * g(x) = current.distance+1   is the path cost to this node
+                 *      (current.distance is the path length to the current
+                 *      node, and turned is current's successor)
+                 * h(x) = heuristic   is the estimated path cost from this node
+                 *                    to the goal
+                 * h(x) NEVER UNDERESTIMATES THE GOAL! THIS IS KEY.
+                 *      Since we KNOW that from here the goal is
+                 *      AT LEAST h(x) moves away, we can throw some
+                 *      turns away
+                 * 
+                 * If f(x) is greater than depth (our current threshold of
+                 * search for the iterative deepening A* search) then we don't
+                 * add it to the stack. BAM!
+                 */
+                int f = heuristic + current.distance + 1;
+                if (f > depth) {
+                    /* 
+                     * Skip the rest of this
+                     * loop iteration
+                     * This goes up to the for loop that loops
+                     * over each turn id.
+                     */
+                    continue;
+                }
+
+                /*
+                 * Now we want to add this turn to the stack, but we're not
+                 * going to do that just yet.  We collect all the turns into
+                 * this array, then sort them as an additional heuristic that
+                 * may or may not be worth the time it takes.
+                 */
+                heuristics[numturns] = f;
+                turns[numturns].cube_data = 
+                /*
+                 * Note to self: I'm stopping here for now.  I want to
+                 * re-work this since the existing code calls uses pointers
+                 * and isn't expensive, but here, qdata holds the actual
+                 * cube string.
+                 * I need to re-think this somehow.  I'm thinking putting the
+                 * turns right into the qdata array as they're generated
+                 * (overwriting them if we need to prune them), and then
+                 * sorting an array of pointers to the qdatas?
+                 * I'll do this later
+                 */
+            }
         }
     }
 
